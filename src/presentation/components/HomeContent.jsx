8283b829a2c9bf51720fd0b842b2cc70d485e8f7
fauzihub13 <fauziadisaputra3@gmail.com>
2025-04-27 14:09:24 +0700

    feat: project detail use dialog and hover effect

--- src/presentation/components/HomeContent.jsx
+++ src/presentation/components/HomeContent.jsx
@@ -102,7 +102,7 @@ export default function HomeContent() {
       <div className="h-10"></div>
       <h1 className="text-2xl font-semibold mb-2">Skills</h1>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2  gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2  gap-4">
         {skills.map((skill, index) => (
           <SkillCard key={index} title={skill.title} techList={skill.tech} />
         ))}
--- src/presentation/components/ProjectCard.jsx
+++ src/presentation/components/ProjectCard.jsx
@@ -1,24 +1,72 @@
 import TechIcon from "./TechIcon";
 
-export default function ProjectCard({project}) {
+export default function ProjectCard({ project }) {
+  const paragraphs = project.description
+    .split("\n")
+    .filter((paragraph) => paragraph.trim() !== "");
+
+  function handleClick() {
+    document.getElementById(project.slug).showModal();
+  }
+
   return (
-    <div className="card bg-base-100  shadow-sm">
-      <figure className="h-60 lg:h-60  overflow-hidden">
-        <img
-          src={`images/projects/${project.thumbnail}`}
-          alt="Shoes"
-          className="w-full h-full object-cover"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{project.title}</h2>
-        <p className="text-zinc-400 mb-2 line-clamp-2">{project.description}</p>
-        <div className="flex flex-wrap gap-2">
-          {project.icons.map((icon, index) => (
-            <TechIcon key={index} path={icon.path} label={icon.label} />
-          ))}
+    <>
+      <div
+        className="group card bg-base-100 shadow-sm cursor-pointer hover:bg-base-200"
+        onClick={handleClick}
+      >
+        <figure className="h-60 lg:h-60 overflow-hidden">
+          <img
+            src={`images/projects/${project.thumbnail}`}
+            alt="Shoes"
+            className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{project.title}</h2>
+          <p className="text-zinc-400 mb-2 line-clamp-2">
+            {project.description}
+          </p>
+          <div className="flex flex-wrap gap-2">
+            {project.icons.map((icon, index) => (
+              <TechIcon key={index} path={icon.path} label={icon.label} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+
+      <dialog id={project.slug} className="modal">
+        <div
+          className="modal-box p-4 w-full"
+          style={{
+            maxHeight: "calc(100vh - 4rem)",
+            overflowY: "auto",
+            WebkitOverflowScrolling: "touch",
+            WebkitScrollbar: { display: "none" },
+            msOverflowStyle: "none",
+            scrollbarWidth: "none",
+          }}
+        >
+          <figure className="h-60 lg:h-60 overflow-hidden">
+            <img
+              src={`images/projects/${project.thumbnail}`}
+              alt="Shoes"
+              className="w-full h-full object-cover rounded-2xl"
+            />
+          </figure>
+
+          <h3 className="font-bold text-lg mt-4">{project.title}</h3>
+
+          {paragraphs.map((paragraph, index) => (
+            <p key={index} className=" py-2 text-justify text-zinc-400 mb-2">
+              {paragraph}
+            </p>
+          ))}
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
   );
 }
