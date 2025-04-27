c4fafbaad3db7795ad3ef627e9d73d5d4e8c3167
fauzihub13 <fauziadisaputra3@gmail.com>
2025-04-27 23:23:26 +0700

    feat: add cv, and can be downloaded or opened

--- src/presentation/components/DownloadCV.jsx
+++ src/presentation/components/DownloadCV.jsx
@@ -1,3 +1,15 @@
 export default function DownloadCV() {
-  return <button className="btn border-base-100 bg-base-200 font-medium">Curriculum Vitae</button>;
+  const cvUrl = "/cv/CV_Fauzi Adi Saputra_Mobile_EN_270425.pdf";
+
+  function handleClick() {
+    window.open(cvUrl, "_blank", "noopener,noreferrer");
+  }
+  return (
+    <button
+      className="btn border-base-100 bg-base-200 font-medium"
+      onClick={handleClick}
+    >
+      Curriculum Vitae
+    </button>
+  );
 }
