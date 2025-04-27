839e649515eb812771f541ac5a81eb8097ce3c48
fauzihub13 <fauziadisaputra3@gmail.com>
2025-04-27 23:29:14 +0700

    fix: remove space on cv url

--- src/presentation/components/DownloadCV.jsx
+++ src/presentation/components/DownloadCV.jsx
@@ -1,7 +1,8 @@
 export default function DownloadCV() {
-  const cvUrl = "/cv/CV_Fauzi Adi Saputra_Mobile_EN_270425.pdf";
+  const cvUrl = "/cv/CV_Fauzi_Adi_Saputra_Mobile_EN_270425.pdf";
 
   function handleClick() {
+    console.log(cvUrl);
     window.open(cvUrl, "_blank", "noopener,noreferrer");
   }
   return (
